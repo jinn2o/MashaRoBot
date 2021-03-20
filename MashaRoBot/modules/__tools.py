695a03f4f3dd62f92042297cd218cb9f005ccb55
jinn2o <79449714+jinn2o@users.noreply.github.com>
2021-03-20 23:26:30 +0300

    Update __tools.py

--- MashaRoBot/modules/__tools.py
+++ MashaRoBot/modules/__tools.py
@@ -6,8 +6,8 @@ __help__ = """
  ❍ /github [username] :Get information about a GitHub user.
  ❍ /country [country name] :Gathering info about given country
  ❍ /imdb [Movie name] :Get full info about a movie with imdb.com
- ❍ Masha [query] :Masha answers the query
-  💡Ex: `Masha where is India?`
+ ❍ JINN [query] :JINN answers the query
+  💡Ex: `JINN where is Chalakudy?`
  ❍ /encrypt :Encrypts The Given Text
  ❍ /decrypt :Decrypts Previously Ecrypted Text
 """
